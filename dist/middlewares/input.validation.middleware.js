32f2cf697510923b08c807c85630396f7df07d47
fateevanastusha <anastasiafateeva2406@gmail.com>
2023-07-03 21:34:04 +0300

    fix of validation

--- dist/middlewares/input.validation.middleware.js
+++ dist/middlewares/input.validation.middleware.js
@@ -15,12 +15,7 @@ const inputValidationMiddleware = (req, res, next) => {
     const error = (0, express_validator_1.validationResult)(req);
     if (!error.isEmpty()) {
         return res.status(400).send({
-            errorsMessages: error.array({ onlyFirstError: true }).map(e => {
-                return {
-                    message: e.msg,
-                    field: e.type
-                };
-            })
+            errorsMessages: error.array({ onlyFirstError: true })
         });
     }
     return next();
@@ -34,15 +29,15 @@ const checkArrayForNumbers = (array) => __awaiter(void 0, void 0, void 0, functi
 });
 exports.checkArrayForNumbers = checkArrayForNumbers;
 //query check
-exports.dateValidation = (0, express_validator_1.query)('date').optional().isString().matches(/^(\d{4}-\d{2}-\d{2})(,\d{4}-\d{2}-\d{2})?$/);
+exports.dateValidation = (0, express_validator_1.query)('date').optional().isString().withMessage('must be string').matches(/^(\d{4}-\d{2}-\d{2})(,\d{4}-\d{2}-\d{2})?$/).withMessage('must be date');
 exports.statusValidation = (0, express_validator_1.query)('status').optional().toInt().isInt({ min: 0, max: 1 });
 exports.teacherIdsValidation = (0, express_validator_1.query)('teacherIds').optional().trim().matches(/^(\d+(\.\d+)?(,\s*\d+(\.\d+)?)*|\d+(\.\d+)?|\d+)$/);
 exports.studentsCountValidation = (0, express_validator_1.query)('studentsCount').optional().trim().matches(/^(\d+(\.\d+)?(,\d+(\.\d+)?)?)$/);
 exports.pageValidation = (0, express_validator_1.query)('page').optional().toInt().isInt({ min: 1 });
 exports.lessonsPerPageValidation = (0, express_validator_1.query)('lessonsPerPage').optional().toInt().isInt({ min: 1 });
 exports.teachersIdsValidation = (0, express_validator_1.body)('teacherIds').isArray().custom(exports.checkArrayForNumbers);
-exports.titleValidation = (0, express_validator_1.body)('title').isString().isLength({ max: 100 });
+exports.titleValidation = (0, express_validator_1.body)('title').isString().withMessage('must be string').isLength({ max: 100 });
 exports.daysValidation = (0, express_validator_1.body)('days').isArray().custom(exports.checkArrayForNumbers);
-exports.firstDateValidation = (0, express_validator_1.body)('firstDate').isString().matches(/^\d{4}-\d{2}-\d{2}$/);
-exports.lastDateValidation = (0, express_validator_1.body)('lastDate').optional().isString().matches(/^\d{4}-\d{2}-\d{2}$/);
+exports.firstDateValidation = (0, express_validator_1.body)('firstDate').isString().withMessage('must be string').matches(/^\d{4}-\d{2}-\d{2}$/).withMessage('must be date');
+exports.lastDateValidation = (0, express_validator_1.body)('lastDate').optional().isString().withMessage('must be string').matches(/^\d{4}-\d{2}-\d{2}$/).withMessage('must be date');
 exports.lessonsCountValidation = (0, express_validator_1.body)('lessonsCount').optional().isInt({ min: 1, max: 300 });
--- src/middlewares/input.validation.middleware.ts
+++ src/middlewares/input.validation.middleware.ts
@@ -5,12 +5,7 @@ export const inputValidationMiddleware = (req: Request, res: Response, next: Nex
     const error = validationResult(req)
     if (!error.isEmpty()) {
         return res.status(400).send({
-            errorsMessages: error.array({onlyFirstError: true}).map(e => {
-                return {
-                    message: e.msg,
-                    field: e.type
-                }
-            })
+            errorsMessages: error.array({onlyFirstError: true})
         })
     }
     return next()
@@ -24,7 +19,7 @@ export const checkArrayForNumbers : CustomValidator = async array => {
 
 //query check
 
-export const dateValidation = query('date').optional().isString().matches(/^(\d{4}-\d{2}-\d{2})(,\d{4}-\d{2}-\d{2})?$/);
+export const dateValidation = query('date').optional().isString().withMessage('must be string').matches(/^(\d{4}-\d{2}-\d{2})(,\d{4}-\d{2}-\d{2})?$/).withMessage('must be date')
 export const statusValidation = query('status').optional().toInt().isInt({ min: 0, max : 1 });
 export const teacherIdsValidation = query('teacherIds').optional().trim().matches(/^(\d+(\.\d+)?(,\s*\d+(\.\d+)?)*|\d+(\.\d+)?|\d+)$/);
 export const studentsCountValidation = query('studentsCount').optional().trim().matches(/^(\d+(\.\d+)?(,\d+(\.\d+)?)?)$/);
@@ -32,8 +27,8 @@ export const pageValidation = query('page').optional().toInt().isInt({ min: 1 })
 export const lessonsPerPageValidation = query('lessonsPerPage').optional().toInt().isInt({ min: 1 });
 
 export const teachersIdsValidation = body('teacherIds').isArray().custom(checkArrayForNumbers);
-export const titleValidation = body('title').isString().isLength({max : 100});
+export const titleValidation = body('title').isString().withMessage('must be string').isLength({max : 100});
 export const daysValidation = body('days').isArray().custom(checkArrayForNumbers);
-export const firstDateValidation = body('firstDate').isString().matches(/^\d{4}-\d{2}-\d{2}$/);
-export const lastDateValidation = body('lastDate').optional().isString().matches(/^\d{4}-\d{2}-\d{2}$/);
+export const firstDateValidation = body('firstDate').isString().withMessage('must be string').matches(/^\d{4}-\d{2}-\d{2}$/).withMessage('must be date');
+export const lastDateValidation = body('lastDate').optional().isString().withMessage('must be string').matches(/^\d{4}-\d{2}-\d{2}$/).withMessage('must be date');
 export const lessonsCountValidation = body('lessonsCount').optional().isInt({min : 1, max : 300});
